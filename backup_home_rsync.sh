0c247b89142df73017ad6f01439eb82efc045ec1
vinz86 <vincenzo.disalvatore@gmail.com>
2024-02-11 19:58:10 +0100

    Fix working dir, clean code

--- backup_home_rsync.sh
+++ backup_home_rsync.sh
@@ -4,23 +4,24 @@ LOG_ENABLED=true
 
 INPUT_FOLDER="/home/$USER"
 BACKUP_FOLDER="/media/$USER/backup"
-#INPUT_FOLDER="/home/$USER/Scaricati/a"
-#BACKUP_FOLDER="/home/$USER/Scaricati/b"
-LOG_FOLDER=$PWD
+LOG_FOLDER="$PWD"
 
-# shellcheck disable=SC2164
-cd "$INPUT_FOLDER"
+CURRENT_DIRECTORY="$PWD"
+
+cd "$INPUT_FOLDER" ||
 if [ $? -eq 1 ]; then
   echo "KO - Errore: $INPUT_FOLDER non esistente: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
   exit 1
 fi
 
-# shellcheck disable=SC2164
-cd "$BACKUP_FOLDER"
+cd "$BACKUP_FOLDER" ||
 if [ $? -eq 1 ]; then
   echo "KO - Errore: $BACKUP_FOLDER non esistente: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
   exit 1
 fi
+
+# Ritorno nella directory dello script
+cd "$CURRENT_DIRECTORY" ||
 
 # -v    --verbose
 # -h    --human-readable
@@ -28,9 +29,9 @@ fi
 # -a    --archive             crea un archivio dei file, compresi gli attributi dei file
 # -i    --itemize-changes     restituisce in output la lista dei cambiamenti
 #       --delete              cancella files estranei dalla cartella di destinazione
-RSYNC_COMMAND=$(rsync -rai  --exclude={'.cache/*','.local/share/Trash','.mozilla/*'} --delete "$INPUT_FOLDER" "$BACKUP_FOLDER")
-#RSYNC_COMMAND=$(rsync -vhrai  --exclude={'.cache/*','.local/share/Trash','.mozilla/*'} --delete "$INPUT_FOLDER" "$BACKUP_FOLDER")
 
+#RSYNC_COMMAND=$(rsync -vhrai  --exclude={'.cache/*','.local/share/Trash','.mozilla/*'} --delete "$INPUT_FOLDER" "$BACKUP_FOLDER")
+RSYNC_COMMAND=$(rsync -rai  --exclude={'.cache/*','.local/share/Trash','.mozilla/*'} --delete "$INPUT_FOLDER" "$BACKUP_FOLDER")
 if [ $? -eq 0 ]; then
   if [ -n "${RSYNC_COMMAND}" ]; then
     sh "$PWD/backup_notify"  -u critical -i dialog-warning "Backup giornaliero" "Backup eseguito: $(date)"

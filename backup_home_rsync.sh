7991edf280ebcbba5450512b80b951211dac6c34
vinz86 <vincenzo.disalvatore@gmail.com>
2024-02-11 09:10:23 +0100

    controllo se esistono le directory

--- backup_home_rsync.sh
+++ backup_home_rsync.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+LOG_ENABLED=true
+
 INPUT_FOLDER="/home/$USER"
 BACKUP_FOLDER="/media/$USER/backup"
-LOG_ENABLED=true
+#INPUT_FOLDER="/home/$USER/Scaricati/a"
+#BACKUP_FOLDER="/home/$USER/Scaricati/b"
 LOG_FOLDER=$PWD
+
+# shellcheck disable=SC2164
+cd "$INPUT_FOLDER"
+if [ $? -eq 1 ]; then
+  echo "KO - Errore: $INPUT_FOLDER non esistente: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
+  exit 1
+fi
+
+# shellcheck disable=SC2164
+cd "$BACKUP_FOLDER"
+if [ $? -eq 1 ]; then
+  echo "KO - Errore: $BACKUP_FOLDER non esistente: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
+  exit 1
+fi
 
 # -v    --verbose
 # -h    --human-readable
@@ -11,26 +28,25 @@ LOG_FOLDER=$PWD
 # -a    --archive             crea un archivio dei file, compresi gli attributi dei file
 # -i    --itemize-changes     restituisce in output la lista dei cambiamenti
 #       --delete              cancella files estranei dalla cartella di destinazione
-
-#RSYNC_COMMAND=$(rsync -vhrai  --exclude={'.cache/*','.local/share/Trash','.mozilla/*'} --delete "$INPUT_FOLDER" "$BACKUP_FOLDER")
 RSYNC_COMMAND=$(rsync -rai  --exclude={'.cache/*','.local/share/Trash','.mozilla/*'} --delete "$INPUT_FOLDER" "$BACKUP_FOLDER")
+#RSYNC_COMMAND=$(rsync -vhrai  --exclude={'.cache/*','.local/share/Trash','.mozilla/*'} --delete "$INPUT_FOLDER" "$BACKUP_FOLDER")
 
 if [ $? -eq 0 ]; then
   if [ -n "${RSYNC_COMMAND}" ]; then
-    sh "$PWD/backup_notify"  -u critical -i dialog-warning "Backup giornaliero" "Il backup giornaliero è stato effettuato: $(date): files modificati"
+    sh "$PWD/backup_notify"  -u critical -i dialog-warning "Backup giornaliero" "Backup eseguito: $(date)"
     if [ ${LOG_ENABLED} = true ]; then
-      echo "Backup eseguito: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
+      echo "OK - Backup eseguito: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
     fi
   else
-    sh "$PWD/backup_notify"  -u critical -i dialog-warning "Backup giornaliero" "Il backup giornaliero è stato effettuato: $(date): nessuna modifica"
+    sh "$PWD/backup_notify"  -u critical -i dialog-warning "Backup giornaliero" "Backup eseguito senza modifiche: $(date)"
     if [ ${LOG_ENABLED} = true ]; then
-      echo "Backup eseguito senza modifiche: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
+      echo "NM - Backup eseguito: nessuna modifica: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
     fi
   fi
 else
   sh "$PWD/backup_notify"  -u critical -i dialog-warning "Backup giornaliero" "Si è verificato un errore: $(date)"
     if [ ${LOG_ENABLED} = true ]; then
-      echo "Errore, backup non eseguito: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
+      echo "KO - Errore: backup non eseguito: $(date)" >> "$LOG_FOLDER/backup_home_rsync.log"
     fi
   exit 1
 fi
